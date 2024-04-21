52310213eb7cb2144b8f065566a2edee12bf3c59
Soolaf Alharky <sal@netcompany.com>
2024-04-22 01:28:52 +0200

    Display a message when the input value is not known

--- weather-widget/src/services/weatherService.ts
+++ weather-widget/src/services/weatherService.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 const API_KEY = 'e20f30d80cbf690ebd2324356880c94e';
 
 export const fetchWeatherData = async (location: string): Promise<any> => {
-  const API_URL = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${API_KEY}`;
+  const API_URL = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${API_KEY}&units=metric`;
 
   try {
     const response = await axios.get(API_URL);
