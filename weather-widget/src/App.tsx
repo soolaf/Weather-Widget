157f69257e93dd28cb6bcb8766820d3f476ab968
Soolaf Alharky <sal@netcompany.com>
2024-04-21 21:23:34 +0200

    Render Header and NavigationBarButtons components in App.tsx

--- weather-widget/src/App.tsx
+++ weather-widget/src/App.tsx
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import NavigationBarButtons from './components/NavigationBarButtons/NavigationBarButtons';
+import Header from './components/Header/Header';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App: React.FC = () => {
+    const [inputValue, setInputValue] = useState('London');
+    const [location, setLocation] = useState('London');
+    const [selectedButton, setSelectedButton] = useState('general');
+
+    const handleFormSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        setLocation(inputValue);
+    };
+
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setInputValue(e.target.value);
+    };
+
+    const onClickButton = (option: string) => {
+      setSelectedButton(option);
+    };
+
+    return (
+        <div className="App">
+            <Header
+                inputValue={inputValue}
+                onInputChange={handleInputChange}
+                onFormSubmit={handleFormSubmit}
+            />
+            <NavigationBarButtons onClickButton={onClickButton} />
+        </div>
+    );
+};
 
 export default App;
